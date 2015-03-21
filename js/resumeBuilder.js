9698d85306ac5b6bc4a1a191f8f4aa266f6bfed2
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 11:33:06 -0400

    Array manipulation: Increased value of last number in array by one

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,24 @@
-var s = "audacity";
+var sampleArray = [0,0,7];
 
-var udacityizer = function(s) {  
-    s = s[1].toUpperCase() + s.slice(2);
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+    // Your code should make newArray equal to an array that has the same
+    // values as _array, but the last number has increased by one.
+    
+    // For example:
+    // _array = [1, 2, 3];
+    // turns into:
+    // newArray = [1, 2, 4];
+    
+    // Your code goes in here!
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
     
     
-    return s;
+    // Don't delete this line!
+    return newArray;
 };
 
-console.log(udacityizer(s));
+// Did your code work? The line below will tell you!
+console.log(incrementLastArrayElement(sampleArray));
