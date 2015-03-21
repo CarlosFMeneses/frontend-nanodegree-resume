9b29c7c230c72bd13176348db244d2448268df13
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 12:03:33 -0400

    String manipulation 2: Capatilized 1st letter of 1st name. Last name is all caps.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,24 @@
-var sampleArray = [0,0,7];
+/*
+If given a string of a two word name formatted with any mix of capitalization, 
+can you manipulate the string to ensure the first name has a capital first 
+letter and the last name is totally capitalized? Your answer should be a single 
+string saved to the variable called finalName.
+*/
 
-var incrementLastArrayElement = function(_array) {
-    var newArray = [];
-    // Your code should make newArray equal to an array that has the same
-    // values as _array, but the last number has increased by one.
-    
-    // For example:
-    // _array = [1, 2, 3];
-    // turns into:
-    // newArray = [1, 2, 4];
-    
-    // Your code goes in here!
-    newArray = _array.slice(0);
-    var lastNumber = newArray.pop();
-    newArray.push(lastNumber + 1);
-    
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+
+    var names = oldName.split(" ");
+    names[1] = names[1].toUpperCase();
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    finalName = names.join(" ");
     
     // Don't delete this line!
-    return newArray;
-};
+    return finalName;
+}
 
 // Did your code work? The line below will tell you!
-console.log(incrementLastArrayElement(sampleArray));
+console.log(nameChanger(name));
