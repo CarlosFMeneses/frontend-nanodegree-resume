96c225d5cdd298858d75f3d035e71d562bdff841
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 13:00:29 -0400

    Created the object "bio" using object literal notation. Used .append() to add bio to the page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,23 @@
-/*
-If given a string of a two word name formatted with any mix of capitalization, 
-can you manipulate the string to ensure the first name has a capital first 
-letter and the last name is totally capitalized? Your answer should be a single 
-string saved to the variable called finalName.
-*/
+var name = "Carlos F. Meneses";
+var role = "Programer";
+var contacts = {
+	"mobile": "(908)505-5097",
+	"email": "carlos.f.meneses@gmail.com",
+	"github": "CarlosFMeneses",
+	"twitter": "@carlosfmeneses",
+	"location": "NYC Metro Area"
+};
+var bioPic = "images/fry.jpg";
+var welcomeMessage = "Greetings and Salutations";
+var skills = ["programing", "designing", "photography", "JS"];
 
-var name = "AlbERt EINstEiN";
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contact" : contacts,
+	"picture" : bioPic,
+	"welcome" : welcomeMessage,
+	"skills" : skills
+};
 
-function nameChanger(oldName) {
-    var finalName = oldName;
-    // Your code goes here!
-
-    var names = oldName.split(" ");
-    names[1] = names[1].toUpperCase();
-    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
-    finalName = names.join(" ");
-    
-    // Don't delete this line!
-    return finalName;
-}
-
-// Did your code work? The line below will tell you!
-console.log(nameChanger(name));
+$("#main").append(bio.name);
