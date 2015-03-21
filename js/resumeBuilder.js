a5335939514c2b50d893dea91b4cde8182005937
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 18:33:33 -0400

    Created education object using object literal notation (JSON)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,31 @@
-var position = "Web Developer";
-var employer = "CueProductions";
-var workYears = 7;
-var cityBusi = "New York";
-
-var work = {};
-work.position = position;
-work.employer = employer;
-work.workYears = workYears;
-work.cityBusi = cityBusi;
-
-var schoolLast = "BCC";
-var schoolYears = 1;
-var schoolCity = "Paramus"
-
-var education = {};
-education["name"] = schoolLast;
-education["schoolYears"] = schoolYears;
-education["schoolCity"] = schoolCity;
-
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+var education = {
+	"schools": [
+		{
+			"name": "Elizabeth High School",
+			"city": "Elizabeth, NJ, USA",
+			"mayor": ["Computer Science", "Graphic Design"],
+			"minor": ["French", "Music Theory"],
+			"year": 1987
+		},
+		{
+			"name": "Center for Media Arts",
+			"city": "New York, NY, USA",
+			"mayor": ["Graphic Design", "Desktop Publishing"],
+			"year": 1994
+		},
+		{
+			"name": "Bergen Community College",
+			"city": "Paramus, NJ, USA",
+			"mayor": "Database Programing",
+			"year": "2015"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Java Programing",
+			"School": "Bergen Community College",
+			"dates": 2014,
+			"url": "http://www.Bergen.edu"
+		}
+	]
+}
