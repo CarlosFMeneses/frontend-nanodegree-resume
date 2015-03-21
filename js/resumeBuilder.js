5718bec66322d9d72954d83ae96f2c55c786e80d
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 13:37:27 -0400

    Added work object using dot notation, and eductaion object using  brackert notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,22 @@
-var name = "Carlos F. Meneses";
-var role = "Programer";
-var contacts = {
-	"mobile": "(908)505-5097",
-	"email": "carlos.f.meneses@gmail.com",
-	"github": "CarlosFMeneses",
-	"twitter": "@carlosfmeneses",
-	"location": "NYC Metro Area"
-};
-var bioPic = "images/fry.jpg";
-var welcomeMessage = "Greetings and Salutations";
-var skills = ["programing", "designing", "photography", "JS"];
+var position = "Web Developer";
+var employer = "CueProductions";
+var workYears = 7;
+var cityBusi = "New York";
 
-var bio = {
-	"name" : name,
-	"role" : role,
-	"contact" : contacts,
-	"picture" : bioPic,
-	"welcome" : welcomeMessage,
-	"skills" : skills
-};
+var work = {};
+work.position = position;
+work.employer = employer;
+work.workYears = workYears;
+work.cityBusi = cityBusi;
 
-$("#main").append(bio.name);
+var schoolLast = "BCC";
+var schoolYears = 1;
+var schoolCity = "Paramus"
+
+var education = {};
+education["name"] = schoolLast;
+education["schoolYears"] = schoolYears;
+education["schoolCity"] = schoolCity;
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
