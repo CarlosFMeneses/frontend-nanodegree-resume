b38214ee748c0908c32b34ee8860c68ecb66de01
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-03-21 21:55:40 -0400

    Created function displayWork(), and place work experience in it

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -96,22 +96,26 @@ if (bio.skills.length > 0) {
 	$("#skills").append(formattedSkill);
 };
 
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+function displayWork() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
 
-	var formattedEmployer = HTMLworkEmployer.replace 
-		("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace 
-		("%data%", work.jobs[job].title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		var formattedEmployer = HTMLworkEmployer.replace 
+			("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace 
+			("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-	$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedEmployerTitle);
 
-	var formattedDates = HTMLworkDates.replace
-		("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDates);
+		var formattedDates = HTMLworkDates.replace
+			("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
 
-	var formattedDescription = HTMLworkDescription.replace
-		("%data%", work.jobs[job].description);
-	$(".work-entry:last").append(formattedDescription);
+		var formattedDescription = HTMLworkDescription.replace
+			("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
 }
+
+displayWork();
